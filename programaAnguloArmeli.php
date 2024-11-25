9c24523f4d25fa6a26ed6be9f3cef690cf5de611
AlejoJoaquin <alejojoaquin011@gmail.com>
2024-11-25 13:28:05 -0300

    se mueve de lugar el llamado y el impirmir la partida

--- programaAnguloArmeli.php
+++ programaAnguloArmeli.php
@@ -234,7 +234,7 @@ do {
 
             $indicesUtilizadas = [];
 
-            do{
+            
                 echo "Por favor, seleccione el numero de la palabra del listado a continuacion\n";
                 foreach ($coleccionPalabras as $index => $palabra) {
                     echo ($index + 1) . ". " . $palabra . "\n"; // 
@@ -249,16 +249,14 @@ do {
                    } else {
                     $indicesUtilizadas[] = $eleccion;
                     $palabraElegida = $coleccionPalabras[$eleccion - 1];
-
-                    $partida = jugarWordix($palabraElegida, strtolower($nombreJugador));
-                     print_r($partida);
                 };
+                
+                $partida = jugarWordix($palabraElegida, strtolower($nombreJugador));
+                print_r($partida);
                 
                 
 
             }
-            }while(true);
-
             break;
         case 2: 
             $coleccionPalabras = cargarColeccionPalabras();
