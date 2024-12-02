b3aee8776bb9bf6cf6638e42226bf3d0d3eb4261
AlejoJoaquin <alejojoaquin011@gmail.com>
2024-12-01 23:46:11 -0300

    se modifica la condicion al final del do while

--- programaAnguloArmeli.php
+++ programaAnguloArmeli.php
@@ -278,7 +278,7 @@ do {
               if ($indiceUtilizado) {
                  echo "Ya has utilizado la palabra número " . $eleccion . ". Por favor, elige otro número.\n";
                 }
-            } while(in_array($eleccion, $partidasPorJugador[$nombreJugador]));
+            } while($indiceUtilizado);
             $partidasPorJugador[$nombreJugador][] = $eleccion;
             $palabraElegida = $coleccionPalabras[$eleccion - 1];
             $partida = jugarWordix($palabraElegida, strtolower($nombreJugador));
